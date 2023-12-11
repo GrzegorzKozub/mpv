980ca492bf7fa1a4060cc74314cec3ffd0dd1c7f
greg <grzegorz.kozub@gmail.com>
2023-12-11 07:52:29 +0100

    init

---
+++ .luacheckrc
@@ -0,0 +1,2 @@
+globals = { 'mp' }
+unused_args = { 'self' }
---
+++ scripts/osc/main.lua
@@ -0,0 +1,34 @@
+-- https://github.com/mpv-player/mpv/blob/master/DOCS/man/lua.rst
+-- https://aegisub.org/docs/latest/ass_tags/
+
+mp.commandv('set', 'osc', 'no')
+
+local elements = require 'elements'
+local mouse = require 'mouse'
+local osd = require 'osd'
+local window = require 'window'
+
+local shown = false
+
+elements.init()
+
+mp.observe_property('osd-dimensions', 'native', function()
+  window.update()
+  osd.setup()
+  elements.refresh()
+  if shown then
+    elements.redraw()
+  end
+end)
+
+mouse.on_move(function()
+  elements.redraw()
+  require('timer').delay(3, function()
+    osd.update ''
+    shown = false
+  end)
+  shown = true
+end)
+
+-- bug: when (un)pausing, the osc shows up and does not hide
+-- bug: mouse cursor does not hide
